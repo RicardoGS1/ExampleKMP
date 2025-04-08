b91c65ac111f69626791f4c0d663ec9c71cef7d0
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-04-08 11:19:35 -0400

    Add padding to menu buttons

--- feature/menu/src/commonMain/kotlin/com/virtualworld/multiplatformiot/feature/menu/MenuScreen.kt
+++ feature/menu/src/commonMain/kotlin/com/virtualworld/multiplatformiot/feature/menu/MenuScreen.kt
@@ -3,11 +3,13 @@ package com.virtualworld.multiplatformiot.feature.menu
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.virtualworld.multiplatformiot.ui.core.localPaddingCommon
 
 
 @Composable
@@ -23,20 +25,25 @@ internal fun MenuScreen(
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
+            val buttonModifier = Modifier.padding( localPaddingCommon.current.normal)
+
             Button(
                 onClick = goToLocalConection,
+                buttonModifier
             ) {
                 Text("LocalConnections")
             }
 
             Button(
                 onClick = goToInternetConection,
+                buttonModifier
             ) {
                 Text("InternetConnections")
             }
 
             Button(
                 onClick = goToBluetoothConection,
+                buttonModifier
             ) {
                 Text("BluetoothConnections")
             }
