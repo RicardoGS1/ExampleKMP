3a99d31ab6b003efe0c745103fcadc3d3199e847
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-04-20 21:56:06 -0400

    remove koin init on iOS side

--- composeApp/src/iosMain/kotlin/com/virtualworld/multiplatformiot/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/virtualworld/multiplatformiot/MainViewController.kt
@@ -1,13 +1,8 @@
 package com.virtualworld.multiplatformiot
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.virtualworld.multiplatformiot.id.initKoin
 
-fun MainViewController() = ComposeUIViewController(configure = {
-
-    initKoin()
-}
-) {
+fun MainViewController() = ComposeUIViewController {
 
 
     App()
