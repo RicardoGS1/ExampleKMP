17ff865119cb8caccf5ba0d1fe6efa21ef657cfe
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-04-24 11:55:17 -0400

    feat: add firebase configuration and dependencies
    This commit introduces Firebase integration into the project.
    
    **iOS:**
    
    -   Added `GoogleService-Info.plist` to the iOS app for Firebase configuration.
    -   Updated the Xcode project file (`project.pbxproj`) to include `GoogleService-Info.plist` as a resource and added the  `XCLocalSwiftPackageReference` to add firebase dependencies
    - Configured firebase initialization in `iOSApp.swift` file, using the `AppDelegate` to make the initialization at the beginning of the application
    
    **Android:**
    
    -   Added the `google-services` plugin to the root `build.gradle.kts`.
    - Added the dependency and the configuration of firebase in `libs.versions.toml`
    -   Initialized Firebase in `MainActivity.kt`.
    - Added internet permission and network state in `AndroidManifest.xml`
    
    **Common:**
    - Added `conectionInternet` module in data to add more features to the project.
    
    **Desktop:**
    - Configured firebase initialization in `main.kt` file.
    - Added a FirebasePlatform implementation to support desktop initialization.
    - Added storage `mutableMapOf` to storage the necessary info to the initialization
    
    **Dependencies:**
    - Added `firebase-bom`, `gitlive-firebase-firestore`, `googleServices` and `androidx-foundation-android` to `libs.versions.toml`.
    - Updated Koin and Ktor dependencies to the latest versions.
    - add `gitlive` dependency to make possible to use `firestore` in common code
    - Corrected the versions of the `koin` dependencies
    - Corrected the versions of the `ktor` dependencies
    - add `androidx-foundation-android` to manage the app in compose

--- composeApp/src/androidMain/kotlin/com/virtualworld/multiplatformiot/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/virtualworld/multiplatformiot/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
 import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Firebase.initialize(this)
 
         setContent {
             App(platformModule = module { single<Context> { applicationContext } })
--- composeApp/src/desktopMain/kotlin/com/virtualworld/multiplatformiot/main.kt
+++ composeApp/src/desktopMain/kotlin/com/virtualworld/multiplatformiot/main.kt
@@ -1,12 +1,39 @@
 package com.virtualworld.multiplatformiot
 
+import android.app.Application
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.google.firebase.FirebasePlatform
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.FirebaseOptions
+import dev.gitlive.firebase.initialize
 
 
 fun main() = application {
 
+    FirebasePlatform.initializeFirebasePlatform(object : FirebasePlatform() {
 
+        val storage = mutableMapOf<String, String>()
+
+        override fun clear(key: String) {
+            storage.remove(key)
+        }
+
+        override fun log(msg: String) = println("gms ggggggg")
+
+        override fun retrieve(key: String): String? = storage[key]
+
+        override fun store(key: String, value: String) = storage.set(key, value)
+
+    })
+
+    val options = FirebaseOptions(
+        projectId = "multiplatformiot",
+        applicationId = "1:909972065182:web:f1bfcb17a32005c9e9e48f",
+        apiKey = "AIzaSyBqVQi5_zsr88KTKG4N9QcQ5GAsslD_Esc",
+    )
+
+    Firebase.initialize(Application(), options)
 
     Window(
         onCloseRequest = ::exitApplication,
