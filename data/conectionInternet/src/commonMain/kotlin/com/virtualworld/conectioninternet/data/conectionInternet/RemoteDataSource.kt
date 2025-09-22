a4f9b81ba4aae1145c1b449de1d9b11e0a97e687
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-09-22 11:22:43 -0400

    refactor: update ArduinoDomain import path and add Bluetooth dependencies
    This commit updates the import path for `ArduinoDomain` model across multiple modules.
    It also introduces Bluetooth permissions in `AndroidManifest.xml` and adds the `android-bluetooth` dependency in `gradle/libs.versions.toml`.
    
    Additionally, the `settings.gradle.kts` file has been updated to include new modules:
    - `:data:connectionBluetooth`
    - `:domain:connectionBluetooth`
    - `:domain:core`

--- data/conectionInternet/src/commonMain/kotlin/com/virtualworld/conectioninternet/data/conectionInternet/RemoteDataSource.kt
+++ data/conectionInternet/src/commonMain/kotlin/com/virtualworld/conectioninternet/data/conectionInternet/RemoteDataSource.kt
@@ -4,7 +4,7 @@ import com.virtualworld.multiplatformiot.ProductEmptyException
 import com.virtualworld.multiplatformiot.data.core.NetworkResponseState
 import com.virtualworld.multiplatformiot.data.core.dto.ArduinoData
 import com.virtualworld.multiplatformiot.data.core.dto.StateObject
-import com.virtualworld.multiplatformiot.domain.conectionInternet.model.ArduinoDomain
+import com.virtualworld.multiplatformiot.domain.core.models.ArduinoDomain
 import dev.gitlive.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
