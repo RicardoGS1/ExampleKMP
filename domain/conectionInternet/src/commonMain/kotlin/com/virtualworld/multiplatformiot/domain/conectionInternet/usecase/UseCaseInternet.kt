8825dcc5d8b737ba663e017f76177402e4f10f3f
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-09-22 11:25:15 -0400

    Refactor: Update ArduinoDomain import path
    This commit updates the import path for `ArduinoDomain` in `UseCaseInternet.kt`.
    
    The import has been changed from:
    `com.virtualworld.multiplatformiot.domain.conectionInternet.model.ArduinoDomain`
    
    to:
    `com.virtualworld.multiplatformiot.domain.core.models.ArduinoDomain`

--- domain/conectionInternet/src/commonMain/kotlin/com/virtualworld/multiplatformiot/domain/conectionInternet/usecase/UseCaseInternet.kt
+++ domain/conectionInternet/src/commonMain/kotlin/com/virtualworld/multiplatformiot/domain/conectionInternet/usecase/UseCaseInternet.kt
@@ -3,7 +3,7 @@ package com.virtualworld.multiplatformiot.domain.conectionInternet.usecase
 
 import com.virtualworld.multiplatformiot.data.core.NetworkResponseState
 import com.virtualworld.multiplatformiot.domain.conectionInternet.mapper.mapperToDomain
-import com.virtualworld.multiplatformiot.domain.conectionInternet.model.ArduinoDomain
+import com.virtualworld.multiplatformiot.domain.core.models.ArduinoDomain
 import com.virtualworld.multiplatformiot.domain.conectionInternet.model.ResponseState
 import com.virtualworld.multiplatformiot.domain.conectionInternet.repository.RepositoryInternet
 import kotlinx.coroutines.flow.Flow
