bb6e5a1a006f9ea81f53825e3e08e3b73684aa88
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-11-03 19:27:23 -0500

    feat: add navigation for Bluetooth LE connection
    This commit introduces the navigation flow for the Bluetooth Low Energy (LE) connection screen.

--- feature/menu/src/commonMain/kotlin/com/virtualworld/multiplatformiot/feature/menu/navigations/MenuRoutes.kt
+++ feature/menu/src/commonMain/kotlin/com/virtualworld/multiplatformiot/feature/menu/navigations/MenuRoutes.kt
@@ -8,26 +8,28 @@ import kotlinx.serialization.Serializable
 import org.koin.compose.viewmodel.koinViewModel
 
 @Serializable
-sealed class MenuNavigation (val route: String){
+sealed class MenuNavigation(val route: String) {
 
     @Serializable
     data object Menu : MenuNavigation("menu")
 }
 
 
-fun NavGraphBuilder.menuRoutes(
+fun NavGraphBuilder.menuRoutesGraph(
     goToLocalConection: () -> Unit,
     goToInternetConection: () -> Unit,
-    goToBluetoothConection: () -> Unit,) {
+    goToBluetoothConection: () -> Unit,
+    goToBluetoothLEConection: () -> Unit,) {
 
     composable(MenuNavigation.Menu.route) {
 
         val viewModel: MenuViewModel = koinViewModel()
-        
+
         MenuScreen(
             goToLocalConection = goToLocalConection,
-            goToInternetConection=goToInternetConection,
-            goToBluetoothConection=goToBluetoothConection,
+            goToInternetConection = goToInternetConection,
+            goToBluetoothConection = goToBluetoothConection,
+            goToBluetoothLEConection = goToBluetoothLEConection,
             menuViewModel = viewModel
         )
 
